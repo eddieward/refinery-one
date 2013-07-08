e18775bb020fce071b29162297318df196d48af7
Eddie Ward <eddie@isle3.com>
2013-07-08 09:18:31 -0700

    adding captcha

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
 
   def notify(quote)
     @quote = quote
-    mail(:to => "eddie@orakatsystems.com", :subject => "Free Quote for " + quote.name, :from => "Engel Tax and Accounting")
+    mail(:to => "bryan@engeltaxandaccounting.com", :subject => "Free Quote for " + quote.name, :from => "Engel Tax and Accounting")
   end
 
   def confirmation(quote)
